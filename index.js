04a41c2c73b6891f2015c41559127de06fd78b9c
Steckenbauer <sten.luhtoja@gmail.com>
2020-05-08 10:13:40 +0300

    fix additional logging

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ http.createServer(function (request, response) {
       data = Buffer.concat(data);
       try {
         let decodedGtfsData = GtfsRealtimeBindings.transit_realtime.FeedMessage.decode(data);
-        console.log('received from post: ', decodedGtfsData);
+        console.log('received from post: ', JSON.stringify(decodedGtfsData));
         sync.syncOtpAndGtfs(JSON.stringify(decodedGtfsData), request.url);
         response.write('received');
       } catch (e) {
