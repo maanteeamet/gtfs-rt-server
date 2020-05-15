b34623b84648daf50b1d3bb32d031eccd255d9c1
Steckenbauer <sten.luhtoja@gmail.com>
2020-05-15 12:08:26 +0300

    ridango dependencies again info.tripUpdate ? info.tripUpdate : info

--- src/otp_match.js
+++ src/otp_match.js
@@ -44,27 +44,30 @@
       const date = new Date();
       let out_info;
 
+
+
       const tripID = info.tripUpdate ? info.tripUpdate.trip.tripId : info.vehicle.trip.tripId;
+      const trip = info.tripUpdate ? info.tripUpdate : info;
       out_info = {
         vehicle: {
-          id: info.vehicle.vehicle.id,
-          label: info.vehicle.vehicle.label,
-          type: info.type
+          id: trip.vehicle.vehicle.id,
+          label: trip.vehicle.vehicle.label,
+          type: trip.type
         },
         trip: {
-          gtfsId: info.gtfsId.split(':')[1],
+          gtfsId: trip.gtfsId.split(':')[1],
           route: tripID,
           direction: 0,
-          operator: info.operator,
-          headsign: info.lineNumber,
+          operator: trip.operator,
+          headsign: trip.lineNumber,
           start_time: ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2)
         },
         position: {
-          latitude: info.vehicle.position.latitude,
-          longitude: info.vehicle.position.longitude,
+          latitude: trip.vehicle.position.latitude,
+          longitude: trip.vehicle.position.longitude,
           //odometer: parseFloat info.distance_from_start
-          speed: info.vehicle.position.speed / 3.6,
-          bearing: info.vehicle.position.heading
+          speed: trip.vehicle.position.speed / 3.6,
+          bearing: trip.vehicle.position.heading
         },
         //delay: -(parseFloat info.difference_from_timetable)
         timestamp: new Date().getTime() / 1000
